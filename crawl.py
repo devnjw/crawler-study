52d6d0c78773f41e8d9810375b4d1016705735bb
21600212 <21600212@handong.edu>
2021-10-28 22:25:02 +0900

    study 2nd

--- crawl.py
+++ crawl.py
@@ -14,5 +14,23 @@ chrome_options.add_argument( '--disable-gpu' )
 driver = webdriver.Chrome( executable_path=DRIVER_PATH, chrome_options=chrome_options )
 driver.get( URL )
 
-print( driver.page_source )
-
+elements = driver.find_elements_by_xpath('//*[@id="layBodyWrap"]/div/div/div[2]/div[2]/section[1]/ul/li')
+ 
+print( '상품 개수: {}'.format( len(elements) ) )
+for el in elements:
+	el_title = el.find_element_by_xpath('div/div[2]/div/div[1]')
+	try:
+		el_price = el.find_element_by_xpath('div/div[2]/div/div[3]')
+	except:
+		el_price = el.find_element_by_xpath('div/div[2]/div/div[2]')
+ 
+	tagName = el_title.tag_name
+	className = el_title.get_attribute('class')
+	title = el_title.text
+	price = el_price.text
+    
+	print( '='*50 )
+	print( 'tagName: {}'.format( tagName ) )
+	print( 'className: {}'.format( className ) )
+	print( 'title: {}'.format( title ) )
+	print( 'price: {}'.format( price ) )
